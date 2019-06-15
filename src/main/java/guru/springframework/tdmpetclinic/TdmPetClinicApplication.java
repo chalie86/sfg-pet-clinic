1e85dd5436ff2f90b06e85e5072fa1e80fc3661e
Tshifhiwa Madima <desmond_madima@yahoo.com>
2019-06-15 12:00:29 +0200

    Initial Project Add

---
+++ src/main/java/guru/springframework/tdmpetclinic/TdmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.tdmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdmPetClinicApplication.class, args);
+    }
+
+}
