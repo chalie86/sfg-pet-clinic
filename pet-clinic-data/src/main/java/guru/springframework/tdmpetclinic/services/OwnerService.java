4a871793b4463a22d5974129bef616f7d031b2fb
Tshifhiwa Madima <desmond_madima@yahoo.com>
2019-08-20 19:56:00 +0200

    pet config

--- pet-clinic-data/src/main/java/guru/springframework/tdmpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/tdmpetclinic/services/OwnerService.java
@@ -2,13 +2,17 @@ package guru.springframework.tdmpetclinic.services;
 
 import guru.springframework.tdmpetclinic.module.Owner;
 
+import java.util.List;
 import java.util.Set;
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
-    Owner findByLastname(String Lastname);
+   /* Owner findByLastname(String Lastname);
     Owner findById(long id);
     Owner Save(Owner owner);
-    Set<Owner> findAll();
+    Set<Owner> findAll();*/
+   Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
