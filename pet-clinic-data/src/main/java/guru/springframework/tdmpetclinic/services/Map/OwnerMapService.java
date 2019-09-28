03f859f5e7a8c7205196fd466b36d5c46a2cb04c
Tshifhiwa Madima <desmond_madima@yahoo.com>
2019-09-28 19:34:54 +0200

    pet

--- pet-clinic-data/src/main/java/guru/springframework/tdmpetclinic/services/Map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/tdmpetclinic/services/Map/OwnerMapService.java
@@ -12,8 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Set;
 
-import java.util.List;
-import java.util.Set;
+
 
 @Service
 @Profile({"default", "map"})
---
+++ pet-clinic-data/src/test/java/guru/springframework/tdmpetclinic/services/Map/OwnerMapServiceTest.java
@@ -0,0 +1,36 @@
+package guru.springframework.tdmpetclinic.services.Map;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+}
